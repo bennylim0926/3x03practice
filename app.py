dc94ef2e605c0b3b19105d53b1a908b349690715
Benny Lim <bennylim0926@gmail.com>
2023-11-14 22:49:46 +0800

    edit

--- app.py
+++ app.py
@@ -2,8 +2,14 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def index():
     return render_template('./index.html')
+
+@app.route('/go', methods=["POST"])
+def go():
+    input = request.form['Input']
+    return render_template('./index.html',log=input),200
+
 
 if __name__ == '__main__':
    app.run(debug=True, port=8001, host="0.0.0.0")
