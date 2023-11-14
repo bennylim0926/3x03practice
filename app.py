bf8dc869553ea681b713318e845b50eaf3f75366
Benny Lim <bennylim0926@gmail.com>
2023-11-14 22:14:53 +0800

    edit

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World'
+    return render_template('./index.html')
 
 if __name__ == '__main__':
    app.run(debug=True, port=8001, host="0.0.0.0")
